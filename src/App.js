df04f48115a3ee6553da0701b7fecc2c02ffa8da
arsimmsejdiu <arsimmsejdiu@gmail.com>
2020-08-30 11:31:37 +0200

    Alan Ai news App build with alan Ide and react in frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import wordsToNumbers from 'words-to-numbers';
 
-function App() {
+import useStyles from "./styles";
+import NewsCards from "./components/NewsCards/NewsCards";
+
+const alanKey =
+  "939ab5c01685cdf40314f7cc2a41f67c2e956eca572e1d8b807a3e2338fdd0dc/stage";
+
+// newaapi.org api key 40c6681fd2ae4ea99cdf1b73543ed39c
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(-1);
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles , number }) => {
+        if (command === "newHeadlines") {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === "highlight") {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if(command === "open") {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number , {fuzzy: true}) : number;
+          const article = articles[[parsedNumber - 1]];
+          
+          if(parsedNumber > 20 ) {
+            alanBtn().playText('Please try that again.');
+          } else if(article) {
+            window.open(article.url, "_blank");
+            alanBtn().playText('Opening ... ')
+          }
+        }
+      },
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src="https://alan.app/voice/images/previews/preview.jpg"
+          className={classes.alanLogo}
+          alt="logo"
+        />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
-}
+};
 
 export default App;
